71e8c18c1ffc569ab1b7e6c5bc331a39d567dca0
Craven-Pirate <hgdebatman@gmail.com>
2024-09-15 18:55:05 +0530

    Changed key details

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,26 +5,26 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
+    title: "Single Security Analytics - Intern",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    place: "Core Analytics Solutions - BlackRock",
+    time: "(May, 2024 - July, 2024)",
+    desp: "<li>Created a proprietary predictive model for improving the analytics for Treasury Futures in Aladdin using data from the exchanges</li> <li>Rectified key analytics like duration & CTD in real time for 80+ Treasury Futures from 5 different exchanges covering 90% global liquidity</li> <li>Engineered and automated cutting-edge analytics workflows, significantly optimizing daily performance insights and boosting profitability for portfolio managers</li>",
   },
   {
-    title: "Student Developer",
+    title: "Quantitative Risk - Intern",
     cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    place: "Risk Management Department - Union Bank of India",
+    time: "(May, 2023 - July, 2023)",
+    desp: "<li>Tasked with evaluating the broad spatial-temporal impact of transition risk on seven sectors in the credit portfolio in the long run</li><li>Designed 6 scenarios prescribed by NGFS incorporating 1200+ different climate-based input variables using GCAM and IIASA data</li><li>Measured radical transformation of impact on probability of default in the range of 4 - 520 basis points across sectors and scenarios</li>",
   },
-  {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
-  },
+  // {
+  //   title: "Research Intern",
+  //   cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
+  //   place: "IIT, Bombay",
+  //   time: "(Dec, 19 - Jan, 20)",
+  //   desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+  // },
 ];
 
 const showCards2 = () => {
