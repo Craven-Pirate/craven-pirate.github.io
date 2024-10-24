e56827483a293cf4f0dcdf232751a4c5ca63da98
Craven-Pirate <hgdebatman@gmail.com>
2024-10-24 14:41:24 +0530

    edu changes

--- assets/js/education.js
+++ assets/js/education.js
@@ -1,5 +1,40 @@
 AOS.init();
 
+
+
+// Certification cards
+
+
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "Financial Risk Manager Part 1",
+    cardImage: "assets/images/experience-page/lstm.png",
+    description:
+      "Secured first quartile in all four subjects in an exam with passing rate of 46%",
+  },
+];
+
+const showCards = () => {
+  let output = "";
+  volunteershipcards.forEach(
+    ({ title, cardImage, description }) =>
+      (output += `        
+      <div class="card volunteerCard" data-aos="fade-down" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="600" style="height: 550px;width:400px">
+      
+      <img src="${cardImage}" height="250" width="65" class="card-img" style="border-radius:10px">
+      <div class="content">
+          <h2 class="volunteerTitle">${title}</h2><br>
+          <p class="copy">${description}</p></div>
+      
+      </div>
+      `)
+  );
+  volunteership.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
 // MOOCs Cards
 
 const moocs = document.querySelector(".moocs");
