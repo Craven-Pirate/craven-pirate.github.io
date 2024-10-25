2fa443e6a445fd48d77cb50c504f23f881d93eac
Craven-Pirate <hgdebatman@gmail.com>
2024-10-26 00:08:56 +0530

    edu

--- assets/js/education.js
+++ assets/js/education.js
@@ -23,7 +23,7 @@ const showPCCards = () => {
       <div class="card volunteerCard" data-aos="fade-down" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="600" style="height: 550px;width:400px">
       
       <img src="${cardImage}" height="250" width="65" class="card-img" style="border-radius:10px">
-      <div class="content">
+      <div class="xcontent">
           <h2 class="volunteerTitle">${title}</h2><br>
           <p class="copy">${description}</p></div>
       
